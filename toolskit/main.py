b818c10f48fe928983a69439570f2e29210b18cc
Crafset <82468619+Crafset@users.noreply.github.com>
2022-11-29 18:26:55 +0100

    fs

---
+++ toolskit/main.py
@@ -0,0 +1,14 @@
+from colorama import Fore, Back, Style
+import requests
+print("┌─────────────────────────┐        ┌─────────────────────────┐        ┌─────────────────────────┐        ┌─────────────────────────┐        ┌─────────────────────────┐")
+print("│  INFORMATION_GATHERING  │        │       NETWORKING        │        │        WEB_APPS         │        │       OBFUSCATION       │        │       UTILITIES         │")
+print("│01- SQLMAP               │        │01- NMAP                 │        │01- XSSTRIKE             │        │01- CUTEIT               │        │01- HOST2IP              │")
+print("│02- STRIKER              │        │02- BETTERCAP            │        │02- PHOTON               │        │                         │        │02- BASE64_DECODE        │")
+print("│03- SUBLIST3R            │        │                         │        │                         │        │                         │        │03- SPAWN_SHELL          │")
+print("│04- SHERLOCK             │        │                         │        │                         │        │                         │        │04- SUGGEST_TOOLS        │")
+print("│05- S3SCANNER            │        │                         │        │                         │        │                         │        │05- CONTRIBUTOR          │")
+print("└─────────────────────────┘        └─────────────────────────┘        └─────────────────────────┘        └─────────────────────────┘        └─────────────────────────┘")
+
+
+
+#print(Back.WHITE +'│information_gathering│')
